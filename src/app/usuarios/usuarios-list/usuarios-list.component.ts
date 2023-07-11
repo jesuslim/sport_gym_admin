f59e6c997cf9288dc2edb44bf89b9d23a4598432
jesuslim <limchuy7@gmail.com>
2023-07-11 00:55:43 -0600

    servicios http y enviroment buggy

--- src/app/usuarios/usuarios-list/usuarios-list.component.ts
+++ src/app/usuarios/usuarios-list/usuarios-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from '../usuarios.service';
 
 @Component({
   selector: 'app-usuarios-list',
   templateUrl: './usuarios-list.component.html',
   styleUrls: ['./usuarios-list.component.css']
 })
-export class UsuariosListComponent {
+export class UsuariosListComponent implements OnInit{
+  users: any[] = [];
+  /**
+   *
+   */
+  constructor(private usuariosService: UsuariosService) {}
+
+  ngOnInit(): void {
+    this.usuarios();
+  }
+
+  usuarios() {
+    this.usuariosService.getUsuarios().subscribe((response) => {
+      this.users = response;
+      console.log(this.users);
+
+    })
+  }
 
 }
--- src/app/usuarios/usuarios.service.ts
+++ src/app/usuarios/usuarios.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
+  apiUrl: any = `${environment.apiUrl}/api/`
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+
+  getUsuarios(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}miembros`);
+  }
 }
