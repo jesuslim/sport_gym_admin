11b7b0de6f3d7e3c257472ab2f2008fbbbe73f7f
jesuslim <limchuy7@gmail.com>
2023-07-16 16:48:39 -0600

    llenado de lista de pagos

--- src/app/pagos/pagos-list/pagos-list.component.ts
+++ src/app/pagos/pagos-list/pagos-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PagosService } from 'src/app/services/pagos.service';
 
 @Component({
   selector: 'app-pagos-list',
@@ -11,11 +12,23 @@ export class PagosListComponent implements OnInit{
   /**
    *
    */
-  constructor( private router: Router) {}
-
+  constructor(
+    private router: Router,
+    private pagosService: PagosService
+    ) {}
+  pagos: any[] = [];
   ngOnInit(): void {
-
+this.findPagos();
   }
+
+  findPagos() {
+    this.pagosService.getPagos().subscribe((response) => {
+      this.pagos = response;
+      console.log('pagos', this.pagos);
+
+    });
+  }
+
 
   toCreate() {
     this.router.navigate(['./dashboard/pagos/create']);
--- src/app/services/pagos.service.ts
+++ src/app/services/pagos.service.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PagosService {
-  apiUrl: any = `${environment.apiUrl}/api/`
+  apiUrl: any = `${environment.apiUrl}/api/pagos`
 
   constructor( private http: HttpClient) { }
 
+  getPagos(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}`);
+  }
 }
